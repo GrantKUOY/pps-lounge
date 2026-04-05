8566e6a8c7d62f29e8ece6483c6a56d0a206fbde
Grant <grant@gm.ahjh.tc.edu.tw>
2026-04-05 09:48:23 +0800

    PPS Lounge 補上網站回報表單

---
+++ pps-feedback-config.js
@@ -0,0 +1,6 @@
+window.PPS_FEEDBACK_CONFIG = {
+  // 將這裡改成你部署好的 Google Apps Script Web App URL，例如：
+  // endpoint: "https://script.google.com/macros/s/xxxxxxxxxxxxxxxx/exec",
+  endpoint: "https://script.google.com/macros/s/AKfycbzJllWoIvuenJldFemRoye7FHpJ3ONxl9A7ud5T5Qf45q8ikQwVkFFfqbtuEXRJRIfM-g/exec",
+  siteName: "PPS Lounge 中文站",
+};
